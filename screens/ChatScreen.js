87ff71e41f16dc391b1cd70e70b99ac5e3b1cd16
Sudipta acharjee <sudiptoacharjee4@gmail.com>
2021-06-04 13:43:38 +0600

    message functionality done

---
+++ screens/ChatScreen.js
@@ -0,0 +1,71 @@
+import React, { useLayoutEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
+import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native';
+
+const ChatScreen = ({ navigation, route }) => {
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Chat",
+            headerBackTitleVisible: false,
+            headerTitleAlign: "left",
+            headerTitle: () => (
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar rounded
+                        source={{
+                            uri:
+                                "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+                        }}
+                    />
+                    <Text
+                        style={{ color: "white", marginLeft: 10, fontWeight: "700" }}
+                    >{route.params.chatName}</Text>
+                </View>
+            ),
+            headerLeft: () => (
+                <TouchableOpacity
+                    style={{ marginLeft: 10 }}
+                    onPress={navigation.goBack}
+                >
+                    <AntDesign name="arrowleft" size={24} color="white" />
+                </TouchableOpacity>
+            ),
+            headerRight: () => (
+                <View
+                    style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        width: 80,
+                        marginRight: 20,
+                    }}
+                >
+                    <TouchableOpacity>
+                        <FontAwesome name="video-camera" size={24} color="white" />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name="call" size={24} color="white" />
+                    </TouchableOpacity>
+                </View>
+            )
+        })
+    }, [navigation])
+
+
+    return (
+        <SafeAreaView style={{flex: 1,backgroundColor:"white"}}>
+            <Text>{route.params.chatName}</Text>
+        </SafeAreaView>
+    )
+}
+
+export default ChatScreen
+
+const styles = StyleSheet.create({})
