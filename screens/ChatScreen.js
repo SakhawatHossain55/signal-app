b6ae2f7bd39c7884c535e35242d80a09e43f90d1
Sudipta acharjee <sudiptoacharjee4@gmail.com>
2021-06-04 14:26:10 +0600

    Icon function done

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,17 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
 import { TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native';
+import { StatusBar } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
+import { Platform } from 'react-native';
+import { ScrollView } from 'react-native';
+import { TextInput } from 'react-native';
 
 const ChatScreen = ({ navigation, route }) => {
+    const [input, setInput] = useState("");
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -58,14 +64,60 @@ const ChatScreen = ({ navigation, route }) => {
         })
     }, [navigation])
 
+    const sendMessage=()=>{
+
+    }
 
     return (
-        <SafeAreaView style={{flex: 1,backgroundColor:"white"}}>
-            <Text>{route.params.chatName}</Text>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+            <StatusBar style="light" />
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.container}
+                keyboardVerticalOffset={90}
+            >
+                <>
+                    <ScrollView>
+
+                    </ScrollView>
+                    <View style={styles.footer}>
+                        <TextInput
+                            value={input}
+                            onChangeText={(text) => setInput(text)}
+                            placeholder="Signal Message"
+                            style={styles.TextInput}
+                        />
+                        <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+                            <Ionicons name="send" size={24} color="#2B68E6" />
+                        </TouchableOpacity>
+                    </View>
+                </>
+
+            </KeyboardAvoidingView>
         </SafeAreaView>
     )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    footer: {
+        flexDirection:"row",
+        alignItems: "center",
+        width:"100%",
+        padding: 15,
+    },
+    TextInput: {
+        bottom:0,
+        height:40,
+        flex:1,
+        marginRight:15,
+        backgroundColor: '#ECECEC',
+        padding:10,
+        color: "grey",
+        borderRadius:30,
+    }
+})
