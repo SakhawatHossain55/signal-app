68b091126a199e9bb67596e10187a363c370a125
Sudipta acharjee <sudiptoacharjee4@gmail.com>
2021-06-04 16:15:54 +0600

    chat-functionality done

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -9,9 +9,14 @@ import { KeyboardAvoidingView } from 'react-native';
 import { Platform } from 'react-native';
 import { ScrollView } from 'react-native';
 import { TextInput } from 'react-native';
+import { Keyboard } from 'react-native';
+import firebase from "firebase";
+import { db, auth } from "../firebase";
+import { TouchableWithoutFeedback } from 'react-native';
 
 const ChatScreen = ({ navigation, route }) => {
     const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([])
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -64,9 +69,33 @@ const ChatScreen = ({ navigation, route }) => {
         })
     }, [navigation])
 
-    const sendMessage=()=>{
+    const sendMessage = () => {
+        Keyboard.dismiss();
 
-    }
+        db.collection('chats').doc(route.params.id).collection('messages').add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            displayName: auth.currentUser.displayName,
+            email: auth.currentUser.email,
+            photoURL: auth.currentUser.photoURL,
+        });
+        setInput("");
+    };
+
+    useLayoutEffect(() => {
+        const unsubscribe = db
+            .collection('chats')
+            .doc(route.params.id)
+            .collection('messages')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot(snapshot => sendMessage(
+                snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    data: doc.data()
+                }))
+            ));
+        return unsubscribe;
+    }, [route])
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
@@ -76,23 +105,41 @@ const ChatScreen = ({ navigation, route }) => {
                 style={styles.container}
                 keyboardVerticalOffset={90}
             >
-                <>
-                    <ScrollView>
-
-                    </ScrollView>
-                    <View style={styles.footer}>
-                        <TextInput
-                            value={input}
-                            onChangeText={(text) => setInput(text)}
-                            placeholder="Signal Message"
-                            style={styles.TextInput}
-                        />
-                        <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
-                            <Ionicons name="send" size={24} color="#2B68E6" />
-                        </TouchableOpacity>
-                    </View>
-                </>
-
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <>
+                        <ScrollView>
+                            {
+                                messages.map(({ id, data }) => (
+                                    data.email === auth.currentUser.email ? (
+                                        <View key={id} style={styles.reciever}>
+                                            <Avatar />
+                                            <Text style={styles.recieverText}>{data.message}
+                                            </Text>
+                                        </View>
+                                    ) : (
+                                        <View style={styles.sender}>
+                                            <Avatar />
+                                            <Text style={styles.senderText}>{data.message}
+                                            </Text>
+                                        </View>
+                                    )
+                                ))
+                            },
+                        </ScrollView>
+                        <View style={styles.footer}>
+                            <TextInput
+                                value={input}
+                                onChangeText={(text) => setInput(text)}
+                                onSubmitEditing={sendMessage}
+                                placeholder="Signal Message"
+                                style={styles.TextInput}
+                            />
+                            <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+                                <Ionicons name="send" size={24} color="#2B68E6" />
+                            </TouchableOpacity>
+                        </View>
+                    </>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
@@ -104,20 +151,30 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    reciever: {
+        padding: 15,
+        backgroundColor: "#ECECEC",
+        alignSelf: "flex-end",
+        borderRadius: 20,
+        marginRight: 15,
+        marginBottom: 20,
+        maxWidth: "80%",
+        postion: "relative",
+    },
     footer: {
-        flexDirection:"row",
+        flexDirection: "row",
         alignItems: "center",
-        width:"100%",
+        width: "100%",
         padding: 15,
     },
     TextInput: {
-        bottom:0,
-        height:40,
-        flex:1,
-        marginRight:15,
+        bottom: 0,
+        height: 40,
+        flex: 1,
+        marginRight: 15,
         backgroundColor: '#ECECEC',
-        padding:10,
+        padding: 10,
         color: "grey",
-        borderRadius:30,
+        borderRadius: 30,
     }
 })
